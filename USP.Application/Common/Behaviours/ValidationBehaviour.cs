8bb10172d1d9ead0269171eb4f235bd2073889f5
katarinamarj <katarinamarjanovic02@gmail.com>
2024-10-04 23:57:56 +0200

    Aktivnost03 Katarina Marjanovic 2021200517

---
+++ USP.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+using MediatR;
+using USP.Application.Common.Exceptions;
+using USP.Application.Common.Extenstions;
+
+namespace USP.Application.Common.Behaviours;
+
+public class ValidationBehaviours<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviours(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+    
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults.Where(r => r.Errors.Count != 0)
+            .SelectMany(r => r.Errors)
+            .ToList();
+
+        if (failures.Any())
+        {
+            throw new UspValidationException(failures.ToGroup());
+        }
+
+        return await next();
+    }
+}
--- USP.Application/DependencyInjection.cs
+++ USP.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
 using System.Reflection;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using USP.Application.Common.Behaviours;
 
 namespace USP.Application;
 
@@ -7,8 +10,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviours<,>));
         return services;
     }
 }
